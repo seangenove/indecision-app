5881b02f12b8352c1a9e44729c30d0c1c76e1c37
seanyboyswag <2150218@slu.edu.ph>
2020-02-01 21:49:17 +0800

    Integrated babel transform-class-properties plugin to allow class properties in classes. Integrated this in AddOption.js and IndecisionApp.js (Component class functions can now be implemented using arrow function notation; calss properties)

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,18 +6,38 @@ import Options from './Options'
 import AddOption from './AddOption'
 
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-
-        this.state = {
-            options: []
-        }
+    state = {
+        options: []
     }
+
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }));
+    };
+
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter(option => optionToRemove !== option)
+        }));
+    };
+
+    handlePick = () =>  {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+
+        alert(option);
+    };
+
+    handleAddOption = (option) => {
+        if (!option) {
+            return "Enter valid value to add item";
+        } else if (this.state.options.indexOf(option) > -1) {
+            return "This option already exists"
+        }
+
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    };
 
     componentDidMount() {
         try {
@@ -30,7 +50,7 @@ export default class IndecisionApp extends React.Component {
         } catch (e) {
             // Do nothing at all 
         }
-    }
+    };
 
     componentDidUpdate(prevProps, prevState) {
         // accessible
@@ -41,41 +61,12 @@ export default class IndecisionApp extends React.Component {
             const json = JSON.stringify(this.state.options);
             localStorage.setItem('options', json)
         }
-    }
+    };
 
     componentWillUnmount() {
         // usually nothing to clean up, barely used
         console.log("Component will unmount");
-    }
-
-    handleDeleteOptions() {
-        this.setState(() => ({ options: [] }));
-    }
-
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter(option => optionToRemove !== option)
-        }));
-    }
-
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum];
-
-        alert(option);
-    }
-
-    handleAddOption(option) {
-        if (!option) {
-            return "Enter valid value to add item";
-        } else if (this.state.options.indexOf(option) > -1) {
-            return "This option already exists"
-        }
-
-        this.setState((prevState) => ({
-            options: prevState.options.concat(option)
-        }));
-    }
+    };
 
     render() {
         const title = "Indecision";
@@ -98,5 +89,5 @@ export default class IndecisionApp extends React.Component {
                 />
             </div>
         )
-    }
+    };
 }
