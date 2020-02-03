1102111f0da9b3e744b7e0cb57440575e7201e26
seanyboyswag <2150218@slu.edu.ph>
2020-02-03 12:47:57 +0800

    Indecision App: Styled 'Add Option' component

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -4,13 +4,20 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
+        closeTimeoutMS={200}
         contentLabel="Selected option"
         ariaHideApp={false}
         onRequestClose={props.handleClearSelectedOption}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <h3
+            className="modal__title"
+        >
+            Selected Option
+        </h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
         <button
+            className="button modal__button"
             onClick={props.handleClearSelectedOption}
         >
             Okay
