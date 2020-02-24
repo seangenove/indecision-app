9fa7d1a8fc340d636fe1bc5a9e5e60800b39ec61
seanyboyswag <2150218@slu.edu.ph>
2020-02-24 16:32:48 +0800

    Added title customizations

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = (props) => (
         <div className="container">
             <h1 className="header__title">{props.title}</h1>
             {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+            <p className="author__subtitle">Developed by Sean Genove</p>
         </div>
     </div>
 );
